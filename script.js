beb7083641652b9ebfd7e1f23c7e72bb39602674
Sheeza Arshad <sheezaarshad94@gmail.com>
2025-02-10 17:50:48 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Add any interactive functionality here if needed
+// For example, smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
